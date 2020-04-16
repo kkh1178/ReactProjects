588b7b57b13abe4b41097a9f1fb2c90a44a82a0b
kkh1178 <kalisha.hill@gmail.com>
2020-04-16 16:17:42 -0500

    pulled geolocation information, determined what season user is in, displayed icons

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,8 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const SeasonDisplay = () => {
-    return <div>Seasonal Display</div>;
+// const seasonConfig = {
+//     summer: {},
+//         text: "let's hit the beach"
+// }
+
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    // Javascript ternary expression, if lat is greater than zero, return summer. otherwise return winter.
+    // logic checks to see if lat is greater than zero is true
+    return lat > 0 ? "summer" : "winter";
+  } else {
+    return lat < 0 ? "winter" : "summer";
+  }
+};
+
+const SeasonDisplay = (props) => {
+  const season = getSeason(props.lat, new Date().getMonth());
+  const text =
+    season === "winter" ? "Brrr, it is chilly!" : "Lets hit the beach!";
+  const icon = season === 'winter' ? 'snowflake' : 'sun';
+  return (
+    <div>
+        {/* backtick - es2015 template string; This similar to using f-strings in python */}
+      <i className={`${icon} icon`} />
+      <h1>{text}</h1>
+      <i className={`${icon} icon`} />
+    </div>
+  );
 };
 
 export default SeasonDisplay;
+
--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,31 +1,48 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
 
 class App extends React.Component {
-// Constructor function particular to javascript not react; 
-// the very first function to be called when a new intance of the component is created.
-    constructor(props) {
-        // We are borrowing functionality from react component base class; Must call super to make sure that we are able to call the constructor function from the parent component
-        // Must be done everytime
-        super(props);
-        // null is used because we don't know what our latitude is yet but we will
-        // This is the only time we do direct assigment to this.state
-        this.state = {lat: null};
-
-        window.navigator.geolocation.getCurrentPosition(
-            (position) => {
-                // To update our state, we called setState. ALways!
-                this.setState({lat: position.coords.latitude});
-                // We never write this.state.lat = position.coords.latitude. Never do direct assignments to this.state
-            },
-            (err) => console.log(err)
-          );
+  // Constructor function particular to javascript not react;
+  // the very first function to be called when a new intance of the component is created.
+  // Generally not recommended to do data loading inside of the constructor; Makes the code clearer than having it spread across componentDidMount and constructor.
+  //   constructor(props) {
+  //     // We are borrowing functionality from react component base class; Must call super to make sure that we are able to call the constructor function from the parent component
+  //     // Must be done everytime
+  //     super(props);
+  //     // null is used because we don't know what our latitude is yet but we will
+  //     // This is the only time we do direct assigment to this.state
+  //     this.state = { lat: null, errorMessage: "" };
+  //   }
+  // This is a shorter way of using JSX to initialize state; Doesn't use a constructor function. Babel will translate it to a constructor function for us.
+  state = { lat: null, errorMessage: "" };
+  // Good place to do data loading!
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      // To update our state, we called setState. ALways!
+      // We never write this.state.lat = position.coords.latitude. Never do direct assignments to this.state
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.message })
+    );
   }
-  
-//   React requires render be defined; it gets called all the time so don't initialize some work or request in render
+
+  // good place to do more data loading when state or props change
+  //   componentDidUpdate() {
+  //     console.log("my component was just updated and re-rendered.");
+  //   }
+
+  // React requires render be defined; it gets called all the time so don't initialize some work or request in render
+  // Render is called twice in this app: once when the application is intiialized and again when we update a property on our state object
   render() {
-    
-    return <div>Latitude: {this.state.lat}</div>;
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat} />;
+    }
+
+    return <div>Loading!</div>;
   }
 }
 
